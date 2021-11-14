e0c442552aa6a4da8be43c09ffa7041073b0097e
aleshya-barbour <aleshya.ab@gmail.com>
2021-11-14 15:44:21 -0500

    message to mesaage

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,21 +1,39 @@
 import React, { Component } from 'react';
+import { commerce } from './lib/commerce';
 
-import { CardProduct } from './Components/Products/Product';
+import './styles/scss/styles.scss'
+import Hero from './Components/Hero';
+import Product from './Components/Products/Product';
 
 class App extends Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      CardProduct: [] 
+      CardProduct: [],
+      cart: {},
     };
   }
+
+  componentDidMount() {
+    this.fetchCart();
+  }
+
+  fetchCart() {
+    commerce.cart.retrieve().then((cart) => {
+      this.setState({ cart });
+    }).catch((error) => {
+      console.error('Issue fetching the cart', error)
+    })
+  }
+ 
 
   render () {
     return (
       <div className="app">
+        <Hero />
       
-      <CardProduct />       
+        <Product />
 
       </div>
     )
--- frontend/src/Components/Products/Product.js
+++ frontend/src/Components/Products/Product.js
@@ -1,23 +1,29 @@
 import React from 'react'
-import {Card, CardMedia, CardContent, Typography, CardActions, Button} from '@mui/material';
+import {Card, Grid, CardMedia, CardContent, Typography, CardActions, Button} from '@mui/material';
 
 
-<img width="100" alt ="kombucha" height="200" src="://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTh9hejjotJbTsLNCMVxIypdKuodVKAB3Pivw&usqp=CAU"></img>
 
-export function CardProduct () {
+export default function CardProduct () {
 
   const products = [
     {Name:"Blood Moon", Description: " berries, spice, and life", Id: 9165, Price:"$3.25", PriceLarge: '$8.99'},
     {Name:"Holiday", Description: "Yellow energy from the sun ", Id:4728, Price:"$3.25", PriceLarge: '$8.99'},
     {Name:"Abundant Earth", Description: " Green is clean", Id:32416 , Price:"$3.25", PriceLarge: '$8.99'},  
+    {Name:"Abundant Earth", Description: " Green is clean", Id:32416 , Price:"$3.25", PriceLarge: '$8.99'},
     {Name:"Seasonal", Description: " Cylce of variables", Id: 71177, Price:"$3.25", PriceLarge: '$8.99'},
     {Name: "Florals for spring? Groundbreaking!", Description: "The notorious floral escape", Id: 59212, Price:"$3.50", PriceLarge: '$9.99', }
   ];
 
   
   return (
+
     <>
+    <Grid  container
+  direction="column"
+  justifyContent="center"
+  alignItems="center" spacing={{ xs: 1, md: 4 }} columns={{ xs: 4, sm: 8, md: 12 }}>
     { products.map((kombuchaDrink) => (
+      <Grid item xs={2} sm={4} md={4} >
        <Card sx={{ maxWidth: 345 }}>
        
         <CardMedia
@@ -27,7 +33,7 @@ export function CardProduct () {
           image="https://media.istockphoto.com/photos/fermented-strawberry-kombucha-tea-with-bottles-picture-id1294202740?b=1&k=20&m=1294202740&s=170667a&w=0&h=8BrBAXD-5uroYOwfeVGGdK610NJhGKPg-mkYZh_ckU4="
         />
         <CardContent>
-         
+          
           <Typography gutterBottom variant="h5" component="div">
             {kombuchaDrink.Name}
           </Typography>
@@ -38,13 +44,22 @@ export function CardProduct () {
             
           </CardContent>
           <CardActions>
-            <Button size="small" color="error" variant="contained">
+            <Button 
+            size="small" 
+            color="error" 
+            variant="contained"
+            name="Add to cart"
+            className="buy__btn"
+            
+  
+            >
               Buy
             </Button>
           </CardActions>
         </Card>
+        </Grid>
         ))}
-           
+      </Grid>
     
   
   </>
